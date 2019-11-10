f7df8f853960597df5ef5c56da7e9923d5e40652
Chiara Deluca <cdeluca@css.edu>
2019-11-10 16:39:12 -0600

    Some Changes Done

---
+++ CurrencyTrader.AdoNet/AsyncTradeStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+	class AsyncTradeStorage : ITradeStorage
+	{
+        private readonly Ilogger logger;
+        private ITradeStorage SynchTradeStorage;
+
+        public AsyncTradeStorage(ILogger loggere)
+        {
+            this.logger = logger;
+            SynchTradeStorage = new AdoNetTradeStorage(logger);
+        }
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            logger.LogInfo("Connecting to Database");
+            SynchTradeStorage.Persist(trades);
+
+        }
+	}
+}
